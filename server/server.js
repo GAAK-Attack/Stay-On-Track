c6cfb8d733bd1be2ce90b28f17dbeed448ef87f9
Garrett Weaver <uncommonweaver@gmail.com>
2021-03-01 13:43:37 -0600

    added boilerplate

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/build', express.static(path.resolve(__dirname, '../build')))
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+module.exports = app;
