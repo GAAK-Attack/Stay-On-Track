c1393157104623d0621a8f4fcd8ea54bab58a6a5
Alvin Cheng <alvincheng50@gmail.com>
2021-03-02 16:48:18 -0500

    added stayOnTrack model and a PNG of the data model

---
+++ server/models/stayOnTrackModel.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+const { PG_URI } = require('../settings.js');
+
+const pool = new Pool({
+  connectionString: PG_URI
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('Executed query:', text);
+    return pool.query(text, params, callback);
+  }
+};
