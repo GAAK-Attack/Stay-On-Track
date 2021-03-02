83cd5b00f912daf8e62e86f721e4199e93e9069b
Alvin Cheng <alvincheng50@gmail.com>
2021-03-02 15:42:53 -0500

    edited populate_default scripts

--- __psqlScripts__/sot_populate_default.sql
+++ __psqlScripts__/sot_populate_default.sql
@@ -9,20 +9,20 @@ INSERT INTO users (username, password, first_name, last_name) VALUES
 
 -- contacts table has contact_id, first_name, last_name, company, email
 INSERT INTO contacts (first_name, last_name, company, email) VALUES
+  ('Mark', 'Zuckerberg', 'Facebook', 'mark.zuckerberg@facebook.com'),
   ('Jeff', 'Bezos', 'Amazon', 'jeff.bezos@amazon.com'),
-  ('Reed', 'Hastings', 'Netflix', 'reed.hastings@netflix.com'),
-  ('Bill', 'Gates', 'Microsoft', 'bill.gates@microsoft.com');
+  ('Tim', 'Cook', 'Apple', 'tim.cook@apple.com');
 
 INSERT INTO contacts (first_name, last_name, company) VALUES
-  ('Mark', 'Zuckerberg', 'Facebook'),
-  ('Tim', 'Cook', 'Apple'),
+  ('Reed', 'Hastings', 'Netflix'),
   ('Sundar', 'Pichai', 'Google');
+  ('Bill', 'Gates', 'Microsoft');
 
 -- engagements table has username, contact_id, time_created, method, status, notes
 INSERT INTO engagements (username, contact_id, method, notes) VALUES
-  ('garrettweaver', 5, 'email', 'emailed tim.cook@apple.com'),
-  ('keithlisiak', 1, 'emailed Jeff Bezos', 'reached out yesterday');
+  ('brucealexandernance', 1, 'email', 'emailed mark.zuckerberg@facebook.com'),
+  ('alvincheng', 4, 'looked up phone number on google and called Sundar Pichai', 'did it yesterday');
 
 INSERT INTO engagements (username, contact_id, method) VALUES
-  ('brucealexandernance', 4, 'LinkedIn messaged Mark Zuckerberg'),
-  ('alvincheng', 6, 'looked up phone number on google and called Sundar Pichai');
+  ('garrettweaver', 2, 'LinkedIn messaged Jeff Bezos'),
+  ('keithlisiak', 5, 'emailed sundar.pichai@google.com');
