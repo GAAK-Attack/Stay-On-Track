488998e98575d05a1f02a5cc9905e3918e7d2075
Alvin Cheng <alvincheng50@gmail.com>
2021-03-02 10:21:34 -0500

    added semicolons

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use('/build', express.static(path.resolve(__dirname, '../build')))
+app.use('/build', express.static(path.resolve(__dirname, '../build')));
 app.use(express.static(path.resolve(__dirname, '../client')));
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+  console.log(`Listening on port ${PORT}`);
 })
 
 module.exports = app;
