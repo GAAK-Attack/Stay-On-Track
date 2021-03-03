633b0a3f07063e081b4acf70fbd96ce805e87443
Alvin Cheng <alvincheng50@gmail.com>
2021-03-03 03:32:27 -0500

    edited test for addUser

--- __tests__/sotController.test.js
+++ __tests__/sotController.test.js
@@ -22,12 +22,24 @@ describe('sotController user table tests', () => {
     return 'mockNext invoked';
   };
 
-  it('sotController.addUser successfully adds a user to the users table', async (done) => {
+  it('sotController.addUser successfully adds a user to the users table and stores username, first_name, and last_name, on res.locals.newUser', async (done) => {
     await sotController.addUser(mockReq, mockRes, mockNext);
+
+    let response = await db.query("SELECT * FROM users WHERE username='testUser'");
+    response = response.rows[0];
+
+    expect(response).toEqual({
+      username: 'testUser',
+      password: 'test',
+      first_name: 'Testing',
+      last_name: 'Tester',
+      interval: null
+    })
 
     expect(mockRes.locals.newUser.username).toBe('testUser');
     expect(mockRes.locals.newUser.first_name).toBe('Testing');
     expect(mockRes.locals.newUser.last_name).toBe('Tester');
+
     done();
   });
 });
