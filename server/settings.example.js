d0dff192cfac2ec8b4075d9af5f5ab50fda43486
Alvin Cheng <alvincheng50@gmail.com>
2021-03-03 02:29:20 -0500

    added another URI to a db for testing

--- server/settings.example.js
+++ server/settings.example.js
@@ -1,6 +1,8 @@
-// PostgreSQL URI to your database
-const PG_URI = '';
+// PostgreSQL URIs to your database
+const PG_PROD_URI = '';
+const PG_TEST_URI = '';
 
 module.exports = {
-  PG_URI
+  PG_PROD_URI,
+  PG_TEST_URI
 }
